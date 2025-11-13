a77d7a6743b10c8f6424a70454c02cb73236edee
Dimitar <dimitarzankovit@gmail.com>
2025-11-13 15:40:27 +0200

    Added ViewSet endpoints

--- api_profiles/urls.py
+++ api_profiles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from api_profiles import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset',views.GreetingViewSet,base_name='hello-viewset')
 
 urlpatterns = [
-	path('hello-view/', views.GreetingAPIView.as_view())
+	path('hello-view/', views.GreetingAPIView.as_view()),
+	path('', include(router.urls)),
 ]
--- api_profiles/views.py
+++ api_profiles/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView #Handling manual HTTP requests
 from rest_framework.response import Response #Handling the JSON output
 from rest_framework import status #Handles the statuses for the requests
+from rest_framework import viewsets #Automatically handles HTTP requests
+
 from api_profiles import serializers
 
-
+#Example of how to use APIView :
 class GreetingAPIView(APIView):
 	#Creating manual HTTP Request using APIView for custom endpoints
 	serializer_class = serializers.GreetingSerializer
@@ -15,6 +17,7 @@ class GreetingAPIView(APIView):
 		return Response({"Greeting":"Hey there!","message":list_of_strings})
 
 	def post(self,request):
+		#Create a hello message including the user's name
 		serializer = self.serializer_class(data=request.data)
 		if serializer.is_valid():
 			name = serializer.validated_data.get('name')
@@ -23,7 +26,7 @@ class GreetingAPIView(APIView):
 		return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 	#Example of how to use the other HTTP requests using APIView:
-	def put(self,request.pk=None) #Primary key used as ID to update object by its ID
+	def put(self,request,pk=None): #Primary key used as ID to update object by its id
 		return Response({'HTTP Method':"PUT"})
 
 	def patch(self,request,pk=None):
@@ -31,4 +34,38 @@ class GreetingAPIView(APIView):
 
 	def delete(self,request,pk=None):
 		return Response({'HTTP Method':"DELETE"})
+
+
+#Example of how to use ViewSet :
+class GreetingViewSet(viewsets.ViewSet):
+	serializer_class = serializers.GreetingSerializer #We can use the same serializer from the APIView example
+	def list(self,request): #GET HTTP request
+		list_of_strings = ["This is another example of HTTP requests - this time using ViewSet",
+						"We will print these lines on GET request",
+						"This is the final string of the list that will be printed"]
+		return Response({'message':"Hello,check out these strings:","strings":list_of_strings})
+
+	def create(self,request): #POST HTTP request
+		#Create a hello message including the user's name
+		serializer = self.serializer_class(data=request.data)
+		if serializer.is_valid():
+			name = serializer.validated_data.get('name')
+			message = f'Hello, how you doing {name}'
+			return Response({"response":message})
+		return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+	#Example of how to use the other HTTP requests using ViewSet:
+	def retrieve(self,request,pk=None): #GET HTTP Request
+		return Response({'HTTP Method':"GET"})
+
+	def update(self,request,pk=None): #PUT HTTP Method
+		return Response({'HTTP Method':'PUT'})
+
+	def partial_update(self,request,pk=None): #PATCH HTTP Method
+		return Response({'HTTP Method':'PATCH'})
 		
+	def destroy(self,request,pk=None): #DELETE HTTP Method
+		return Response({'HTTP Method':'DELETE'})
+
+
+
